032d5d14442782f2460f3c50e8017856e9689970
Pulkit Garg <pulkitgarg007@gmail.com>
2020-06-01 18:01:24 +0530

    Api gateway for all microservices

---
+++ src/test/java/om/pulkit/microservices/zuul/apigateway/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package om.pulkit.microservices.zuul.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
